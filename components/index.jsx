c1d0cf18bdc110f9ed7f79b8a3cf71f0db224445
czdujb <13377688576@ctyun.cn>
2015-10-13 16:34:57 +0800

    添加antd项目联合调试的css，添加antd在ie8的patch

--- components/index.jsx
+++ components/index.jsx
@@ -21,6 +21,20 @@ function callback(key) {
 
 var text = " A dog is a type of domesticated animal.";
 
+var Pagination = antd.Pagination;
+
+function onChange(page) {
+  console.log(page);
+}
+
+var Tree = antd.Tree;
+var TreeNode = Tree.TreeNode;
+
+function handleCheck(info) {
+  console.log('check: ', info);
+}
+
+
 
 var MainPage = React.createClass({
 
@@ -30,17 +44,41 @@ var MainPage = React.createClass({
         	<div>
         		<button type="button">This is a button</button>
         		<antd.Datepicker />
+        		<Pagination showQuickJumper={true} onChange={onChange} total={500} />
+        		<Tree defaultExpandAll={true} checkable={true} onCheck={handleCheck}>
+				    <TreeNode title="parent 1">
+				      <TreeNode title="leaf" />
+				      <TreeNode title="parent 1-1">
+				        <TreeNode title="parent 2-1">
+				          <TreeNode title="leaf" />
+				          <TreeNode title="leaf" />
+				        </TreeNode>
+				        <TreeNode title="leaf" />
+				      </TreeNode>
+				    </TreeNode>
+				  </Tree>
         		<Collapse defaultActiveKey={["1"]} onChange={callback}>
-			    <Panel header={`This is panel header 1`} key="1">
-			      <p>{text}</p>
-			    </Panel>
-			    <Panel header={`This is panel header 2`} key="2">
-			      <p>{text}</p>
-			    </Panel>
-			    <Panel header={`This is panel header 3`} key="3">
-			      <p>{text}</p>
-			    </Panel>
-			  </Collapse>
+				    <Panel header={`This is panel header 1`} key="1">
+				      <p>{text}</p>
+				    </Panel>
+				    <Panel header={`This is panel header 2`} key="2">
+				      <p>{text}</p>
+				    </Panel>
+				    <Panel header={`This is panel header 3`} key="3">
+				      <p>{text}</p>
+				    </Panel>
+			  	</Collapse>
+
+			  	<button className="ant-btn">Button</button>
+				<a href="javascript:;" className="ant-btn" role="button">Link</a>
+				<input className="ant-btn" type="button" value="Input" />
+				<input className="ant-btn" type="submit" value="Submit" />
+
+				<br/>
+
+				<button className="ant-btn ant-btn-primary">主按钮</button>
+				<button className="ant-btn">次按钮</button>
+				<button className="ant-btn ant-btn-ghost">幽灵按钮</button>
         	</div>
         	);
     }
